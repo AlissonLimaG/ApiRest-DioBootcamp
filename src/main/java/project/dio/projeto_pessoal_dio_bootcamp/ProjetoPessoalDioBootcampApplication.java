a729203d965756f6f6c37671efe579dc95683572
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-02-25 22:02:56 -0300

    Initial Commit

---
+++ src/main/java/project/dio/projeto_pessoal_dio_bootcamp/ProjetoPessoalDioBootcampApplication.java
@@ -0,0 +1,13 @@
+package project.dio.projeto_pessoal_dio_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoalDioBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPessoalDioBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/dio/projeto_pessoal_dio_bootcamp/ProjetoPessoalDioBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package project.dio.projeto_pessoal_dio_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPessoalDioBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
