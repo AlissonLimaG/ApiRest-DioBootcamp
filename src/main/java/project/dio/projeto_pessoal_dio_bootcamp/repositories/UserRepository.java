64ac91db6faa75251a4c03dd619e93c8fff65bfb
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-02-26 23:34:41 -0300

    fix: alterando query findByAccountByNumber para o correto :findByAccount_Number

--- src/main/java/project/dio/projeto_pessoal_dio_bootcamp/repositories/UserRepository.java
+++ src/main/java/project/dio/projeto_pessoal_dio_bootcamp/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package project.dio.projeto_pessoal_dio_bootcamp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import project.dio.projeto_pessoal_dio_bootcamp.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
-    User findByAccount(String account);
+    User findByAccount_Number(String account);
     
 }
