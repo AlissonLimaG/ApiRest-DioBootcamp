88f6989d476402228691011209a583716ea3945a
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-02-27 02:33:10 -0300

    feat: ControllerAdvice implementado, exceptionsHandler para usuário existente, usuário inexistente e exceções gerais implementados, adição de mensagem personalizada no body e logs no terminal com @Sljf4j do lombok.

---
+++ src/main/java/project/dio/projeto_pessoal_dio_bootcamp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package project.dio.projeto_pessoal_dio_bootcamp.exceptions;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import project.dio.projeto_pessoal_dio_bootcamp.exceptions.businessExceptions.UserAlreadyExistsException;
+import project.dio.projeto_pessoal_dio_bootcamp.exceptions.businessExceptions.UserNotExistsException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(exception = UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorMesage> userAlreadyExists(UserAlreadyExistsException e){
+        log.info(e.getMessage(),e);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        ErrorMesage mesage = new ErrorMesage(HttpStatus.CONFLICT.value(), LocalDateTime.now().format(formatter), e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(mesage);
+    }
+
+    @ExceptionHandler(exception = UserNotExistsException.class)
+    public ResponseEntity<ErrorMesage> userNotExists(UserNotExistsException e){
+        log.error(e.getMessage(),e);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        ErrorMesage mesage = new ErrorMesage(HttpStatus.NOT_FOUND.value(), LocalDateTime.now().format(formatter), e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(mesage);
+    }
+
+    @ExceptionHandler(exception = Exception.class)
+    public ResponseEntity<ErrorMesage> unexpectedException(Exception e){
+        log.error(e.getMessage(),e);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        ErrorMesage mesage = new ErrorMesage(HttpStatus.NOT_FOUND.value(), LocalDateTime.now().format(formatter), e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(mesage);
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    public class ErrorMesage{
+        private Integer status;
+        private String time;
+        private String mesage;
+    }
+
+}
+
