c72c44b185bff90eacc04fbb168e73d30635c3c4
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-02-27 15:14:42 -0300

    chore: configurando perfil de produção para conectar a banco de dados rodando no railway

--- src/main/java/project/dio/projeto_pessoal_dio_bootcamp/ProjetoPessoalDioBootcampApplication.java
+++ src/main/java/project/dio/projeto_pessoal_dio_bootcamp/ProjetoPessoalDioBootcampApplication.java
@@ -3,10 +3,17 @@ package project.dio.projeto_pessoal_dio_bootcamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default server url")})
 @SpringBootApplication
 public class ProjetoPessoalDioBootcampApplication {
 
 	public static void main(String[] args) {
+		System.out.println("PG_URL: " + System.getenv("PG_URL"));
+		System.out.println("PG_USERNAME: " + System.getenv("PG_USERNAME"));
+		System.out.println("PG_PASSWORD: " + System.getenv("PG_PASSWORD"));
 		SpringApplication.run(ProjetoPessoalDioBootcampApplication.class, args);
 	}
 
