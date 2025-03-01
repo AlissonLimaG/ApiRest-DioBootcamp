6403f96ab33084c2b4ec28d422c9785a97c029f3
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-03-01 01:38:34 -0300

    fix: Concertando query sql

--- src/main/java/project/dio/projeto_pessoal_dio_bootcamp/models/User.java
+++ src/main/java/project/dio/projeto_pessoal_dio_bootcamp/models/User.java
@@ -23,7 +23,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "VARHCAR(10) DEFAULT 'ROLE_USER'")
+    @Column(columnDefinition = "VARCHAR(10) DEFAULT 'ROLE_USER'")
     @Enumerated(EnumType.STRING)
     private UserRole role;
 
